3e737c436162a28b8ef5de8fb2aa5a4f43474933
disork420 <gostomskiszymon@gmail.com>
2023-08-03 17:16:25 +0200

    add solution Quadrants CheckSameCase

---
+++ 4.Quadrants/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static int GetQuadrant(int x, int y)
+    {
+        if (x > 0 && y > 0)
+        {
+            // First quadrant
+            return 1;
+        }
+        else if (x < 0 && y > 0)
+        {
+            // Second quadrant
+            return 2;
+        }
+        else if (x < 0 && y < 0)
+        {
+            // Third quadrant
+            return 3;
+        }
+        else
+        {
+            // Fourth quadrant
+            return 4;
+        }
+    }
+}
