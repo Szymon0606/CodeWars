caecf794cb6eb8cce378a6b1aaeedb55d0d08a47
disork420 <gostomskiszymon@gmail.com>
2023-08-03 16:12:48 +0200

    add solution NoBoringZero

---
+++ 1. NoBoringZeros/Program.cs
@@ -0,0 +1,26 @@
+﻿public class NoBoring
+{
+    public static int NoBoringZeros(int number)
+    {
+        if (number == 0)
+        {
+            return 0;
+        }
+        while (number % 10 == 0)
+        {
+            number /= 10;
+        }
+        return number;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 2020, 900010, 120, -20010 };
+
+        foreach (int number in numbers)
+        {
+            int result = NoBoringZeros(number);
+            Console.WriteLine($"{number}->{result}");
+        }
+    }
+}
