b2928973e722d8912c5d5de59e5f5bcce4305859
disork420 <gostomskiszymon@gmail.com>
2023-08-07 21:39:30 +0200

    Add solution  NoBoringZero

---
+++ 6.CountingSheeps/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string CountSheep(int n)
+    {
+        string sheep = "";
+        for (int i = 1; i <= n; i++)
+        {
+            sheep += $"{i} sheep...";
+
+        }
+        return sheep;
+
+    }
+}
