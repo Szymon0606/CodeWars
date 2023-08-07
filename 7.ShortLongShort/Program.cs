286699968266c3f373fa67c34fd03f16eec37c62
disork420 <gostomskiszymon@gmail.com>
2023-08-07 21:41:47 +0200

    Add solution ShortLongShort

---
+++ 7.ShortLongShort/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+class Program
+{
+    static string Solution(string a, string b)
+    {
+        return a.Length > b.Length ? b + a + b : a + b + a;
+    }
+
+    static void Main(string[] args)
+    {
+        string input1 = "5";
+        string input2 = "11";
+
+        string result = Solution(input1, input2);
+        
+    }
+}
