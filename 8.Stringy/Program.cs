cf6249b0af5af5338d48e0f7f3568b75987d61cf
disork420 <gostomskiszymon@gmail.com>
2023-08-07 21:58:13 +0200

    add solution Stringy

---
+++ 8.Stringy/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Kata
+{
+    public static string Stringy(int size)
+    {
+        char[] result = new char[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = i % 2 == 0 ? '1' : '0';
+        }
+
+        return new string(result);
+
+    }
+}
