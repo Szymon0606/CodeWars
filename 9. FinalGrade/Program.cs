33464cd4d386a4b65b00ca1d7c6c9f5d147d9dac
disork420 <gostomskiszymon@gmail.com>
2023-08-09 15:27:06 +0200

    Final Grades

---
+++ 9. FinalGrade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    public class Grades
+    {
+        public static int FinalGrade(int exam, int projects)
+        {
+            if (exam > 90||projects>10)
+            {
+                return 100;
+            }
+            else if (exam > 75 && projects >= 5)
+            {
+                return 90;
+            }
+            else if (exam > 50  && projects >= 2)
+            {
+                return 75;
+            }
+            else
+            {
+                return 0;
+            }
+        }       
+    }
+}
